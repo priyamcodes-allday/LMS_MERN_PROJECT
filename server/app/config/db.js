f5c2b5f0ba81cf5775d7aef1b3d05bf03f011cf5
1234danav <ranitpramanik1234@gmail.com>
2026-06-18 13:57:46 +0530

    server structure

--- server/app/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose')
-
-const databaseConnection=async()=>{
-    mongoose.connect(process.env.MONGODB_URL)
-    console.log('databse connected successfully')
-}
-module.exports=databaseConnection
